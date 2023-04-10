17a7a9d1595aabefba86f988e05ff860ce253b34
01subhashree <ssubhashree094@gmail.com>
2023-04-10 20:39:44 +0530

    table project

---
+++ src/components/Table.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import style from './Table.module.css'
+
+const Table = () => {
+  const [number, setNumber] = useState([2, 5]);
+  const changeNumber = () => {
+    setNumber([...number, Math.round(Math.random() * 100)]);
+    console.log(number);
+  };
+  return (
+    <>
+      <table className={style.table}>
+        <thead >
+          <tr>
+            <th>Number</th>
+          </tr>
+        </thead>
+        <tbody>
+            {number.map((elem, index) => (
+                <tr key={index}>
+                  <td>{elem}</td>
+                </tr>
+            ))}
+        </tbody>
+      </table>
+      <button onClick={changeNumber}>Generate Number</button>
+    </>
+  );
+};
+export default Table;
