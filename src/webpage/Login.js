d6025c22e14ce7a4dd1ae5a0d04cfd595edae53b
01subhashree <ssubhashree094@gmail.com>
2023-04-16 13:58:52 +0530

    Router Webpage

--- src/webpage/Login.js
+++ src/webpage/Login.js
@@ -5,6 +5,7 @@ import style from './Login.module.css'
 
 
 const Loginpage = () => {
+
     return (
         <div className={style.wrapper}>
             <div>
@@ -12,10 +13,10 @@ const Loginpage = () => {
             </div>
             <div className={style.container1}>
                 <div className={style.container2}>
-
                     <input placeholder="Username" className={style.input}
                     />
-                    <input type="password" placeholder="Password" />
+                    <input type="password" placeholder="Password"
+                    />
                     <div className={style.box}>
                         <button className={style.Button}>Login</button>
                         <span>
@@ -25,7 +26,7 @@ const Loginpage = () => {
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 export default Loginpage
